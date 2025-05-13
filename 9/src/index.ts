a411c0dfdc17403fffd2e4bca54eb3712a7c4f59
Chinmaya Garg <ch745692@dal.ca>
2025-05-13 12:56:15 -0300

    Type Any Explained

---
+++ 9/src/index.ts
@@ -0,0 +1,7 @@
+let salesY = 123_456_789;
+let courseY = 'TypeScript';
+let isPublishedY: boolean = true;
+let levelY;
+// We can set value of a variable with type "any" dynamically, TS won't give error.
+levelY = 'a';
+levelY = 1;
